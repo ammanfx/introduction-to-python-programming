4f1503590020c3858c248873910ce6f75b3b6079
Muhammad Abubakar <muhammadabubakarsani123@gmail.com>
2024-11-17 18:02:57 +0100

    python input

---
+++ 0x00-hello_world/2-input.py
@@ -0,0 +1,5 @@
+#!/env/python3
+"""A python script that takes input from user"""
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print(f"Your name is {name} and you're {age} years old.")
