5e5e035f58b4909955a717e5ab60d076bd7acc05
Muhammad Abubakar <muhammadabubakarsani123@gmail.com>
2024-11-23 10:39:21 +0100

    assignment

---
+++ 0x00-hello_world/2-variable.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+"""A script that prints diffrents types of variable"""
+num = 1.324
+name = "Muhammad"
+age = 18
+
+print(type(name))
+print(age)
+print(num)
---
+++ 0x00-hello_world/3-output.py
@@ -0,0 +1,7 @@
+#!/bin/env python3
+"""A python script that takes output from user"""
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+address = input("Enter your adress: ")
+country = input("Enter your country: ")
+print(f"Your name is {name} and you are {age} years old, you stay at {address} and you're from {country} ")
