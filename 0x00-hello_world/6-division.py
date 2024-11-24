bba68d62cb06ae4c8000e023684c32e8cbf75c5d
Muhammad Abubakar <muhammadabubakarsani123@gmail.com>
2024-11-24 08:55:16 +0100

    control structure

---
+++ 0x00-hello_world/6-division.py
@@ -0,0 +1,8 @@
+#!/bin/env python3
+"""A script that print diffrent variables"""
+
+#division
+print(50/2)
+num1 = int(input("enter any number of your choice: "))
+num2 = int(input("enter any number of your choice: "))
+print(num1/num2)
