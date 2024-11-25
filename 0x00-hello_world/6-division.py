32b762c5f1175e0ef6f95d97c70bdffcc0191324
Muhammad Abubakar <muhammadabubakarsani123@gmail.com>
2024-11-25 07:43:43 +0100

    control structure

--- 0x00-hello_world/6-division.py
+++ 0x00-hello_world/6-division.py
@@ -3,6 +3,6 @@
 
 #division
 print(50/2)
-num1 = int(input("enter any number of your choice: "))
-num2 = int(input("enter any number of your choice: "))
+num1 = float(input("enter any number of your choice: "))
+num2 = float(input("enter any number of your choice: "))
 print(num1/num2)
